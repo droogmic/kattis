6811713c58bfe70e48bb6f1bf3339429758deff3
Michael Droogleever Fortuyn <droogmic@gmail.com>
2022-03-23 23:28:27 +0100

    Dead End

--- spiderman/src/main.rs
+++ spiderman/src/main.rs
@@ -1,5 +1,8 @@
 use std::{
-    cmp::Ordering::{Equal, Greater, Less},
+    cmp::{
+        max,
+        Ordering::{Equal, Greater, Less},
+    },
     io::{self, Read},
 };
 
@@ -27,11 +30,7 @@ pub fn func(input: String) -> String {
                 .collect::<Vec<usize>>()
         })
         .map(|distances| {
-            let paths = search(&distances);
-            if let Some(best_path) = paths
-                .into_iter()
-                .min_by_key(|path| *path.iter().max().unwrap())
-            {
+            if let Some(best_path) = search(&distances) {
                 std::iter::once("U")
                     .chain(best_path.windows(2).map(|w| match w[0].partial_cmp(&w[1]) {
                         Some(Less) => "U",
@@ -51,44 +50,43 @@ pub fn func(input: String) -> String {
 }
 
 /// Returns paths
-pub fn search(stages: &[usize]) -> Vec<Vec<usize>> {
-    search_rec(0, stages)
-        .into_iter()
-        .map(|mut v| {
-            v.reverse();
-            v
-        })
-        .collect()
+pub fn search(stages: &[usize]) -> Option<Vec<usize>> {
+    search_rec(0, stages).map(|(_, mut p)| {
+        p.pop();
+        p.reverse();
+        p
+    })
 }
 /// Returns an inverted vector, first in vec is the last distance
-fn search_rec(start: usize, stages: &[usize]) -> Vec<Vec<usize>> {
+fn search_rec(start: usize, stages: &[usize]) -> Option<(usize, Vec<usize>)> {
     match (stages.first(), stages.get(1..stages.len())) {
         (Some(&next_stage), Some([])) => {
             if start == next_stage {
-                vec![vec![]]
+                Some((start, vec![start]))
             } else {
-                vec![]
+                None
             }
         }
         (Some(&next_stage), Some(next_stages)) => {
-            let mut paths = Vec::new();
-            if next_stage <= start {
-                let next = start - next_stage;
-                let down_paths = search_rec(next, next_stages);
-                for mut down_path in down_paths {
-                    down_path.push(next);
-                    paths.push(down_path);
-                }
-            }
-            if start {
-                let next = start + next_stage;
-                let up_paths = search_rec(next, next_stages);
-                for mut up_path in up_paths {
-                    up_path.push(next);
-                    paths.push(up_path);
-                }
-            }
-            paths
+            let down_path = if let Some(down_next) = start.checked_sub(next_stage) {
+                search_rec(down_next, next_stages)
+            } else {
+                None
+            };
+            let up_path = search_rec(start + next_stage, next_stages);
+            let (prev_max, mut p) = match (down_path, up_path) {
+                (None, None) => None,
+                (Some(path), None) => Some(path),
+                (None, Some(path)) => Some(path),
+                (Some(down), Some(up)) => match down.0.partial_cmp(&up.0) {
+                    Some(Less) => Some(down),
+                    Some(Greater) => Some(up),
+                    Some(Equal) => Some(down), // pick any
+                    None => panic!(),
+                },
+            }?;
+            p.push(start);
+            Some((max(start, prev_max), p))
         }
         _ => unreachable!(),
     }
@@ -100,8 +98,9 @@ mod tests {
 
     #[test]
     fn test_search() {
-        assert_eq!(search(&[10, 10]), [[10]]);
-        assert_eq!(search(&[10, 10, 10, 10]), [[10, 0, 10], [10, 20, 10]]);
+        assert_eq!(search(&[10, 10]), Some(vec![10]));
+        assert_eq!(search(&[10, 10, 10, 10]), Some(vec![10, 0, 10]));
+        assert_eq!(search(&[3, 4, 2, 1, 6, 4, 5]), None);
     }
 
     #[test]
